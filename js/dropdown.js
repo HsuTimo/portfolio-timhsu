71df2f06893a6f0839e6216ebf10f5e8cc95f8f3
HsuTimo <72869492+HsuTimo@users.noreply.github.com>
2021-04-29 14:58:57 +0200

    drop down now closes on window resize

--- js/dropdown.js
+++ js/dropdown.js
@@ -15,9 +15,13 @@ function hide() {
 document.getElementById("hamburger").addEventListener("click", function () {
     hideUnHide();
 });
+
 let dropdownLinks = document.getElementsByClassName("dropdown-link");
 for (let i = 0; i < dropdownLinks.length; i++) {
     dropdownLinks[i].addEventListener("click", function () {
         hide();
     });
 }
+window.addEventListener('resize', function () {
+    hide();
+});
